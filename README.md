18bc7ed3a3d62a130775ab0e928a6a7571c881ef
ProdigalP <prodigalprogrammer7@gmail.com>
2024-01-09 23:42:50 +0000

    Project completed. localStorage features implemented successfully
    git status

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# daily-planner-app
+# Daily Planner App
+
+<img src="assets/images/daily-planner-app.jpg" width="700px">
+
+## Description
+
+In this project I was required to create a daily planner app, much like Google Calendar. This app allows the user to set out their day by saving their events in the time blocks.
+
+Once each event is saved they will persist even if the browser tab is closed or refreshed.
+
+_Find the link to the the deployed website [here](https://prodigalprogrammer.github.io/)_
+
+<p align="right">(<a href="#daily-planner-app" >Back to top</a>)</p>
+
+## Usage
+
+This app has a very accurate clock and updates every second. Once you scroll down to the time blocks, you will see they are colour-coded to show which hour in the day you are currently on plus the past and future events.
+
+When you save your event, there is a confirmation message at the top and lets you know which hour you saved your event in. The save button also changes to green to display your last choice. There is also a message if you try to enter a blank event. This app will prohibit you from doing so.
+
+<p align="right">(<a href="#daily-planner-app" >Back to top</a>)</p>
+
+## Features
+
+### jQuery
+
+- Learned a lot through jQuery. Very powerful API that allows you to do so much with so little code.
+
+### LocalStorage
+
+- This app would not be possible without `localStorage` and a permenant place to store all the events.
+
+### For Loop
+
+- The For Loop made creating this a lot easier as many parts of the code are repetitive.
+
+<p align="right">(<a href="#daily-planner-app" >Back to top</a>)</p>
+
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+## Contact
+
+Project Link: [https://github.com/prodigalProgrammer/daily-planner-app](https://github.com/prodigalProgrammer/daily-planner-app)
+
+Monrone Stewart: [YouTube](https://www.youtube.com/@ProdigalP) - prodigalprogrammer7@gmail.com
+
+<p align="right">(<a href="#daily-planner-app" >Back to top</a>)</p>
+
+## Acknowledgement
+
+- [GitHub Pages](https://pages.github.com/)
+- [The Complete JavaScript Course 2024: From Zero to Expert!](https://www.udemy.com/course/the-complete-javascript-course/)
+
+<p align="right">(<a href="#daily-planner-app" >Back to top</a>)</p>
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
 var dateEl = $("#currentDay");
+dateEl.css({
+  fontFamily: 'Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif',
+  fontSize: "2.5em",
+});
 var container = $(".container");
 
 function time() {
@@ -16,6 +20,7 @@ for (var i = 0; i < 9; i++) {
   hour.css({ width: "100px", lineHeight: "80px" });
   hour.text(`${i + 9}:00`);
   var text = $("<textarea>");
+  text.attr("class", `text-area${i + 1}`);
   text.css({ width: "1120px" });
   var save = $("<button class='saveBtn'>");
   save.css({ width: "100px" });
@@ -23,6 +28,7 @@ for (var i = 0; i < 9; i++) {
   row.append(hour);
   row.append(text);
   row.append(save);
+
   var icon = $(
     '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V173.3c0-17-6.7-33.3-18.7-45.3L352 50.7C340 38.7 323.7 32 306.7 32H64zm0 96c0-17.7 14.3-32 32-32H288c17.7 0 32 14.3 32 32v64c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V128zM224 288a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>'
   );
@@ -37,6 +43,17 @@ for (var i = 0; i < 9; i++) {
   }
 }
 
+function getEvents() {
+  for (
+    var x = 1;
+    x <= document.querySelector(".container").childElementCount;
+    ++x
+  ) {
+    $(`.text-area${x}`).val(localStorage.getItem(`eventItem${x}`));
+  }
+}
+getEvents();
+
 document
   .querySelector(".container")
   .addEventListener("click", function (event) {
@@ -45,11 +62,23 @@ document
     saveMsg.classList.add("saveMsg");
     document.querySelector(".container").prepend(saveMsg);
     console.log(element);
+    var textarea = element.previousElementSibling;
     if (element.matches("button") || element.matches("svg")) {
-      saveMsg.textContent =
-        `${parseInt(element.closest("section").dataset.index) + 8}` +
-        ":00 Event Saved! ✅";
+      if (!textarea.value.trim()) {
+        saveMsg.textContent = "Cannot save empty events ❌";
+      } else {
+        saveMsg.textContent =
+          `${parseInt(element.closest("section").dataset.index) + 8}` +
+          ":00 Event Saved! ✅";
+        localStorage.setItem(
+          `eventItem${element.closest("section").dataset.index}`,
+          textarea.value.trim()
+        );
+      }
+      document.querySelector(".container");
       setTimeout(() => (saveMsg.textContent = ""), 2000);
+      console.log(textarea.value.trim());
+      getEvents();
     }
   });
 
